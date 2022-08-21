e624291d3193dfd553db3745357759a0d3be9c2f
lorenzo774 <lorenzoconti774@gmail.com>
2022-08-22 00:11:33 +0200

    Refactor tilemap rects finder algorithm

---
+++ src/core/components/tilemap/tilemap-rects-finder.ts
@@ -0,0 +1,94 @@
+import { Tilemap } from "./tilemap.js";
+import { Rect } from "../../utils/rect.js";
+import { rectVsRect } from "../../physics/swept-functions.js";
+import { Vector2 } from "../../math/vector2.js";
+import { TilemapNodePool } from "./tilemap-node-pool.js";
+
+export class TilemapRectsFinder {
+    constructor(private tilemap: Tilemap) {}
+
+    /**
+     * Algorithm to get individual rects in the map
+     * @returns Array of individual Rects
+     */
+    public find(): Rect[] {
+        let rects = new Array<Rect>();
+
+        for (let i = 0; i < this.tilemap.map[0].length; i += 1) {
+            for (let j = 0; j < this.tilemap.map.length; j += 1) {
+                // Check indexes collision with other rects
+                let collision = false;
+                for (const rect of rects) {
+                    if (
+                        rectVsRect(
+                            new Rect(new Vector2(i, j), Vector2.ONE),
+                            rect
+                        )
+                    ) {
+                        collision = true;
+                    }
+                }
+                if (collision) {
+                    continue;
+                }
+                // Start finding node pool
+                if (this.tilemap.map[j][i] !== -1) {
+                    const nodePool: TilemapNodePool = this.getNodePool(
+                        new Vector2(i, j),
+                        rects
+                    );
+                    const rect = nodePool.getRect();
+                    rects.push(rect);
+                }
+            }
+        }
+        return rects;
+    }
+
+    /**
+     * Algorithm used by getIndividualRects to find an array of nodes
+     * @returns Array of node
+     */
+    private getNodePool(startPos: Vector2, rects: Rect[]): TilemapNodePool {
+        let yCount = 0;
+        let nodePool: TilemapNodePool = new TilemapNodePool();
+
+        for (let i = startPos.x; i < this.tilemap.map[0].length; i += 1) {
+            const tile = this.tilemap.map[startPos.y + yCount][i];
+
+            if (tile === -1) {
+                i = startPos.x;
+                yCount += 1;
+                if (startPos.y + yCount >= this.tilemap.map.length) {
+                    return nodePool;
+                }
+
+                // Check indexes collision with other rects
+                let collision = false;
+                for (const rect of rects) {
+                    if (
+                        rectVsRect(
+                            new Rect(
+                                new Vector2(i, startPos.y + yCount),
+                                Vector2.ONE
+                            ),
+                            rect
+                        )
+                    ) {
+                        collision = true;
+                    }
+                }
+                if (collision) {
+                    break;
+                }
+
+                if (this.tilemap.map[startPos.y + yCount][i] === -1) {
+                    return nodePool;
+                }
+            }
+            nodePool.addNode(new Vector2(i, startPos.y + yCount));
+        }
+
+        return nodePool;
+    }
+}
--- src/core/components/tilemap/tilemap.ts
+++ src/core/components/tilemap/tilemap.ts
@@ -1,12 +1,10 @@
 import { Component } from "../../component.js";
 import { Entity } from "../../entity.js";
-import { Vector2 } from "../../math/vector2.js";
-import { rectVsRect } from "../../physics/swept-functions.js";
 import { Rect } from "../../utils/rect.js";
 import { TilemapBody } from "../bodies/tilemapbody.js";
 import { TilemapDrawer } from "./tilemap-drawer.js";
-import { TilemapNodePool } from "./tilemap-node-pool.js";
 import { Tileset } from "./tileset.js";
+import { TilemapRectsFinder } from "./tilemap-rects-finder.js";
 
 export class Tilemap extends Component {
     private tilemapDrawer: TilemapDrawer;
@@ -15,7 +13,7 @@ export class Tilemap extends Component {
     constructor(
         entity: Entity,
         public tileset: Tileset = null,
-        public map: number[][] = [[]]
+        public map: number[][] = new Array<Array<number>>()
     ) {
         super(entity);
         this.maxRowLength = this.getMaxLength();
@@ -36,91 +34,10 @@ export class Tilemap extends Component {
     }
 
     /**
-     * Algorithm to get individual rects in the map
-     * @returns Array of individual Rects
+     * @returns Individual rects in a tilemap with an algorithm
      */
     public getIndividualRects(): Rect[] {
-        let rects = new Array<Rect>();
-
-        for (let i = 0; i < this.map[0].length; i += 1) {
-            for (let j = 0; j < this.map.length; j += 1) {
-                // Check indexes collision with other rects
-                let collision = false;
-                for (const rect of rects) {
-                    if (
-                        rectVsRect(
-                            new Rect(new Vector2(i, j), Vector2.ONE),
-                            rect
-                        )
-                    ) {
-                        collision = true;
-                    }
-                }
-                if (collision) {
-                    continue;
-                }
-                // Start finding node pool
-                if (this.map[j][i] !== -1) {
-                    const nodePool: TilemapNodePool = this.getNodePool(
-                        new Vector2(i, j),
-                        rects
-                    );
-                    const rect = nodePool.getRect();
-                    rects.push(rect);
-                }
-            }
-        }
-        return rects;
-    }
-
-    /**
-     * Algorithm used by getIndividualRects to find an array of nodes
-     * @returns Array of node
-     */
-    private getNodePool(startPos: Vector2, rects: Rect[]): TilemapNodePool {
-        let yCount = 0;
-        let nodePool: TilemapNodePool = new TilemapNodePool();
-
-        for (let i = startPos.x; i < this.map[0].length; i += 1) {
-            const tile = this.map[startPos.y + yCount][i];
-
-            if (tile === -1) {
-                i = startPos.x;
-                yCount += 1;
-                if (startPos.y + yCount >= this.map.length) {
-                    return nodePool;
-                }
-
-                // Check indexes collision with other rects
-                let collision = false;
-                for (const rect of rects) {
-                    if (
-                        rectVsRect(
-                            new Rect(
-                                new Vector2(i, startPos.y + yCount),
-                                Vector2.ONE
-                            ),
-                            rect
-                        )
-                    ) {
-                        collision = true;
-                    }
-                }
-                if (collision) {
-                    break;
-                    console.log(collision);
-                    continue;
-                }
-
-                if (this.map[startPos.y + yCount][i] === -1) {
-                    return nodePool;
-                }
-            }
-
-            nodePool.addNode(new Vector2(i, startPos.y + yCount));
-        }
-
-        return nodePool;
+        return new TilemapRectsFinder(this).find();
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
