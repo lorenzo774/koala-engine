3f7b2122d1570eb7f3ee0a016f9faa4bd57a3f32
lorenzo774 <lorenzoconti774@gmail.com>
2022-09-04 23:59:41 +0200

    Ignore src js

--- src/editor/HTML/HTMLGenerator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HTMLGenerator = void 0;
-class HTMLGenerator {
-    static create(value) {
-        return "";
-    }
-}
-exports.HTMLGenerator = HTMLGenerator;
--- src/editor/systems/FSM/settings-state.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SettingsState = void 0;
-const editor_state_js_1 = require("./editor-state.js");
-class SettingsState extends editor_state_js_1.EditorState {
-}
-exports.SettingsState = SettingsState;
